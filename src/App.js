6fa9399570bc8546f0b0e96706725ce86c9fe8ff
Student_PSXGA136 <deablosf@hotmail.com>
2020-08-14 09:54:16 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Projects from './components/Projects';
+import Blog from './components/Blog';
+import About from './components/About';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <div>
+    <Router>
+      <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="linkin">
+        <h1>S & B</h1>
+          <div className="topLinks"><Link to="/blog">BLOG</Link></div>
+          <div className="topLinks"><Link to="/projects">PROJECT: Powerpunk</Link></div>
+          <div className="topLinks"><Link to="/about">ABOUT</Link></div>
+        </div>
+
+        </header>
+        <div className="title">
+
+        </div>
+         {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL.  */}
+         <Switch>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/blog">
+            <Blog />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route> 
+        </Switch> 
+      </div>
+    </Router>
+ 
+    <div className="footer">
+    <p> <a className="footerLinks" href="">powered by puppyrecipe</a></p>
+            <p><a className="footerLinks" href="">Built by Sam</a></p>
     </div>
+
+
+
+  </div>
   );
 }
 
---
+++ src/components/Projects.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Projetcs extends Component {
+    render() {
+        return (
+            <div className="ProjMain">
+                <div className="landingImg">
+
+                </div>
+                <div className="elePitch">
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Projetcs
