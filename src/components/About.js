6f5d174fb80f7a10717d0d838f74f6bfe5ab043a
Student_PSXGA136 <deablosf@hotmail.com>
2020-08-16 21:14:26 -0400

    30 percent done

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class About extends Component {
+    render() {
+        return (
+            <div className="about">
+                <h1>WHO ARE WE?!</h1>
+                <p className="wordVomit">Words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words. </p>
+            </div>
+        )
+    }
+}
+
+export default About
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,7 +8,20 @@ class Projetcs extends Component {
 
                 </div>
                 <div className="elePitch">
+                    <h1 className="titles">PROJECT: POWERPUNK</h1>
+                    <p className="wordVomit">Words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words.</p>
+                    <h1 className="titles">THE WORLD</h1>
+                    <p className="wordVomit">Words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words.</p>
+                    <h1 className="titles">EXIM</h1>
+                    <p className="wordVomit">Words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words.</p>
+                </div>
 
+                <div className="theCity">
+                <h1 className="titles">THE CITY</h1>
+                    <p className="wordVomit">Words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words words.</p>
+                    <div className="map"> 
+
+                    </div>
                 </div>
             </div>
         )
